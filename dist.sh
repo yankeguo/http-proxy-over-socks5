b0b5c76f87326fdfa67c63a67389ec8dfb41af8f
GUO YANKE <hi@guoyk.xyz>
2024-01-02 17:29:59 +0800

    ci: add dist.sh

---
+++ dist.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eu
+
+cd "$(dirname "${0}")"
+
+rm -rf dist && mkdir dist
+
+EXECUTABLE_NAME="http-proxy-over-socks5"
+
+build() {
+  rm -rf build && mkdir build
+  CGO_ENABLED=0 GOOS=${1} GOARCH=${2} go build -o "build/${EXECUTABLE_NAME}${3}"
+  tar -czvf "dist/${EXECUTABLE_NAME}-${1}-${2}.tar.gz" --exclude ".*" -C build "${EXECUTABLE_NAME}${3}"
+  rm -rf build
+}
+
+build linux amd64 ""
+build windows amd64 ".exe"
+build darwin amd64 ""
+build darwin arm64 ""
+
+build linux arm64 ""
+build linux loong64 ""
+build windows 386 ".exe"
+build freebsd amd64 ""
+
+cd dist
+
+shasum -a 256 *.tar.gz >SHASUM256.txt
+gpg-trezor-hi@guoyk.xyz -ab SHASUM256.txt
